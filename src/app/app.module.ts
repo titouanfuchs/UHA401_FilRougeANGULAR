fa28f45eed249b3e4bfc5d25992d6a37a3913354
titouan.fuchs <titouan.fuchs@uha.fr>
2021-10-06 14:08:41 +0200

    ajout de l'album card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GroupCardComponent } from './group-card/group-card.component';
+import { AlbumCardComponent } from './album-card/album-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GroupCardComponent
+    GroupCardComponent,
+    AlbumCardComponent
   ],
   imports: [
     BrowserModule,
