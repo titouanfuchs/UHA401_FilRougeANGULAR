c325284e0714f7bce1dc04f547170ea93b3c0094
Doomiprane <38530673+HaagenProd@users.noreply.github.com>
2021-10-15 18:48:45 +0200

    ajout des chargements (pour le moment c'est simulé)

--- src/app/admin-view/admin-view.component.ts
+++ src/app/admin-view/admin-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-admin-view',
@@ -6,8 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-view.component.scss']
 })
 export class AdminViewComponent implements OnInit {
-
   constructor() { }
+
+  callBDDAction(action:number){
+    let triggerLoadButton: any = document.getElementById("triggerLoadModal");
+    let triggerResumeButton: any = document.getElementById("triggerResumeModal");
+    switch (action){
+      case 0:
+        console.log("Calling ClearBDD");
+        if (triggerLoadButton != null){
+          triggerLoadButton.click();
+          setTimeout(() =>{
+            if (triggerResumeButton != null){
+              triggerResumeButton.click();
+            }
+          }, 5000)
+        }
+        break;
+      case 1:
+        console.log("Calling FillBDD");
+        break;
+    }
+  }
 
   ngOnInit(): void {
   }
