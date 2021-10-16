5aae22f3ef2b976582cc826cccff0624245453ef
titouan.fuchs <titouan.fuchs@uha.fr>
2021-10-16 11:47:44 +0200

    les tables ça continue

--- src/app/db-view/db-view.component.ts
+++ src/app/db-view/db-view.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class DbViewComponent implements OnInit {
 
   keys = ["id", "test", "test2", "test3"]
+  lenght = (100/this.keys.length).toString() + "%";
 
   constructor() { }
 
